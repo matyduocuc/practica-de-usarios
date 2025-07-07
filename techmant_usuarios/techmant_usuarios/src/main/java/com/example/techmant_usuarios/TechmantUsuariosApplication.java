aa3a5d3daff19534427e8414787203084a87817a
maty <mati.gaetet@duocuc.cl>
2025-07-07 14:46:39 -0400

    Add files via upload

---
+++ techmant_usuarios/techmant_usuarios/src/main/java/com/example/techmant_usuarios/TechmantUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.techmant_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmantUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmantUsuariosApplication.class, args);
+	}
+
+}
---
+++ techmant_usuarios/techmant_usuarios/src/test/java/com/example/techmant_usuarios/TechmantUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techmant_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmantUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
